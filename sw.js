118376dfe701fef9a264353fa50e5311d97d5805
greenmachine25 <christopher.gammello@gmail.com>
2026-04-29 00:14:40 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'finance-hub-v1';
+const ASSETS = [
+  './finance_app.html',
+  './manifest.json',
+  './icon.png',
+  './favicon.png'
+];
+
+// Install Service Worker and Cache Assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Serve Cached Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
